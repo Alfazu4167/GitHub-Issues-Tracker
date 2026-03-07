ce0f3d9cd6e13a72d4cfc02f252e3d6e63b0e65c
Alfazu4167 <alfazuddin4167@gmail.com>
2026-03-07 17:29:29 +0600

    navbar,card section btn section added

--- script/login.js
+++ script/login.js
@@ -1,1 +1,14 @@
-console.log("Hi I'am JS");
+document.getElementById('singin-btn')
+.addEventListener("click",()=>{
+    const singInInput = document.getElementById("singIn-input")
+    const username = singInInput.value;
+    const passwordInput = document.getElementById("password-input")
+    const password = passwordInput.value;
+
+    if (username == 'admin' && password == "admin123") {
+         alert('Sign in success');
+        window.location.assign('./home.html');
+    }else{
+        alert("Invalid Username or Password")    }
+
+})
