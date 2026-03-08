bf6d23e035fe2eedae9498254a4500e952c594f7
Alfazu4167 <alfazuddin4167@gmail.com>
2026-03-08 21:21:13 +0600

    border top applied based on status

--- script/home.js
+++ script/home.js
@@ -12,39 +12,39 @@ const loadIssues = async () => {
     counter()
 }
 
-const loadIssueDetails =async (id)=> {
+const loadIssueDetails = async (id) => {
 
 
-const url = `https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`
-const res = await fetch(url)
-const data = await res.json();
-displayIssueDetails(data.data);
+    const url = `https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`
+    const res = await fetch(url)
+    const data = await res.json();
+    displayIssueDetails(data.data);
 
 
 }
 
-const displayIssueDetails = (details=>{
+const displayIssueDetails = (details => {
     console.log(details);
-//     {
-//     "id": 3,
-//     "title": "Update README with installation instructions",
-//     "description": "The README file needs better installation instructions for new contributors.",
-//     "status": "closed",
-//     "labels": [
-//         "documentation"
-//     ],
-//     "priority": "low",
-//     "author": "mike_docs",
-//     "assignee": "sarah_dev",
-//     "createdAt": "2024-01-10T08:00:00Z",
-//     "updatedAt": "2024-01-12T16:45:00Z"
-// }
+    //     {
+    //     "id": 3,
+    //     "title": "Update README with installation instructions",
+    //     "description": "The README file needs better installation instructions for new contributors.",
+    //     "status": "closed",
+    //     "labels": [
+    //         "documentation"
+    //     ],
+    //     "priority": "low",
+    //     "author": "mike_docs",
+    //     "assignee": "sarah_dev",
+    //     "createdAt": "2024-01-10T08:00:00Z",
+    //     "updatedAt": "2024-01-12T16:45:00Z"
+    // }
     const issueDetails = document.getElementById("details-container");
     issueDetails.innerHTML = `
     <div class="p-4 space-y-5">
                         <h3 class="text-2xl font-bold">${details.title}</h3>
                         <div class="flex gap-2">
-                            <div class="">${details.status == "open"? "<span class='badge badge-success text-white'>Opened</span>":"<span class='badge bg-red-500 text-white'>Closed</span>"}</div>
+                            <div class="">${details.status == "open" ? "<span class='badge badge-success text-white'>Opened</span>" : "<span class='badge bg-red-500 text-white'>Closed</span>"}</div>
                             <p>. <span class="text-[#64748b]">Opened by${details.author}</span>
                             <p>. <span class="text-[#64748b]">${details.createdAt}</span></p>
 
@@ -62,7 +62,7 @@ const displayIssueDetails = (details=>{
                         <div class="flex justify-between items-center">
                             <div>
                                 <p class="text-[#64748b]">Assignee:</p>
-                                <h2 class="text-lg font-bold">${details.assignee}</h2>
+                                <h2 class="text-lg font-bold">${details.assignee ? details.assignee : "Not fund"}</h2>
                             </div>
                             <div class="space-y-1">
                                 <p class="text-[#64748b]">Priority</p>
@@ -118,9 +118,16 @@ const displayIssues = (issues) => {
 
         const card = document.createElement("div")
         card.className = "card p-4 shadow-md space-y-4"
-        card.onclick = ()=> (loadIssueDetails(issue.id))
+        card.onclick = () => (loadIssueDetails(issue.id))
         card.id = "card-container"
-        
+
+        if (issue.status =="open") {
+            const cardContainer = document.getElementById('card-container')
+            card.classList.add("border-t-8", "border-t-green-400")
+        } else {
+            const cardContainer = document.getElementById('card-container')
+            card.classList.add("border-t-8", "border-t-purple-400")
+        }
         card.innerHTML = `
        <div class="flex justify-between">
                     <div >
@@ -141,7 +148,7 @@ const displayIssues = (issues) => {
                 </div>
                 <hr class="border-gray-200 px-8">
                 <div class="space-y-3">
-                    <p class="text-[#64748b]">${issue.author}</p>
+                    <p class="text-[#64748b]">${issue.author = "undefined" ? issue.author : "Not fund"}</p>
                     <p class="text-[#64748b]">${issue.createdAt}</p>
                 </div>
               `
@@ -149,15 +156,8 @@ const displayIssues = (issues) => {
 
         issuesContainer.appendChild(card)
     });
-    
-    
-// if (openIssues) {
-//         const cardContainer = document.getElementById('card-container')
-//         cardContainer.classList.add("")
-//     }else{
-//         const cardContainer = document.getElementById('card-container')
-//         cardContainer.classList.add("border-t-2 border-t-purple-400")
-//     }
+
+
 }
 loadIssues()
 
